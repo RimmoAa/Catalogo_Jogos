ac08e5cdec1f2996412d5843075ef6354040f41a
RimmoAa <160778703+RimmoAa@users.noreply.github.com>
2025-06-11 15:38:24 -0300

    Melhorias na página inicial: novo design e jogos em destaque

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,17 +1,46 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonButton } from '@ionic/angular/standalone';
-import { NavController } from '@ionic/angular'; // Importe NavController
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon } from '@ionic/angular/standalone';
+import { NavController } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonButton],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, CommonModule],
 })
 export class Tab1Page {
-  constructor(private navCtrl: NavController) {} // Injete NavController
+  
+  featuredGames = [
+    {
+      title: 'Omori',
+      genre: 'RPG Psicológico',
+      rating: 4.9,
+      image: 'assets/featured/omori.jpg'
+    },
+    {
+      title: 'Deltarune',
+      genre: 'RPG Aventura',
+      rating: 4.8,
+      image: 'assets/featured/deltarune.jpg'
+    },
+    {
+      title: 'Sonic Adventure 2',
+      genre: 'Plataforma 3D',
+      rating: 4.5,
+      image: 'assets/featured/sonic_adventure_2.jpg'
+    },
+    {
+      title: 'Hollow Knight',
+      genre: 'Metroidvania',
+      rating: 4.9,
+      image: 'assets/featured/hollow_knight.jpg'
+    }
+  ];
+
+  constructor(private navCtrl: NavController) {}
 
   goToGamesList() {
-    this.navCtrl.navigateForward('/tabs/tab2'); // Use NavController para navegar
+    this.navCtrl.navigateForward('/tabs/tab2');
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,15 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+import { addIcons } from 'ionicons';
+import { star, gameController, search, trendingUp } from 'ionicons/icons';
+
+addIcons({
+  star,
+  'game-controller': gameController,
+  search,
+  'trending-up': trendingUp
+});
+
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
